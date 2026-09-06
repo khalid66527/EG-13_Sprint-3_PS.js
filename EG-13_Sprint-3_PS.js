a3f7e9bb4dcf6fca630fa8645c3560f5e0487829
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-06 20:11:53 +0600

    Move Zeroes

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,7 +1,32 @@
 
+//02. Move Zeroes
+var moveZeroes = function(nums) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    while (index < nums.length) {
+        nums[index] = 0;
+        index++;
+    }
+};
+
+let nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+// console.log(nums);
+
+
+
+
+
+
 // 01. Contains Duplicate
 var containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length;
 };
 
-console.log(containsDuplicate([1, 2, 3, 1]));
