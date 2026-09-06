57626033ba831b4d4bff38a0f4e030f983d72aed
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-06 20:16:30 +0600

    Majority Element

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,71 +1,88 @@
 
-
-
-//04. Ransom Note
-var canConstruct = function(ransomNote, magazine) {
+//05. Majority Element
+var majorityElement = function(nums) {
     let count = {};
 
-    for (let char of magazine) {
-        count[char] = (count[char] || 0) + 1;
-    }
+    for (let num of nums) {
+        count[num] = (count[num] || 0) + 1;
 
-    for (let char of ransomNote) {
-        if (!count[char]) {
-            return false;
-        }
-
-        count[char]--;
-    }
-
-    return true;
-};
-
-
-
-
-
-//03. Valid Anagram
-var isAnagram = function(s, t) {
-    return s.split("").sort().join("") === t.split("").sort().join("");
-};
-
-
-
-
-
-
-
-
-
-
-//02. Move Zeroes
-var moveZeroes = function(nums) {
-    let index = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            nums[index] = nums[i];
-            index++;
+        if (count[num] > nums.length / 2) {
+            return num;
         }
     }
-
-    while (index < nums.length) {
-        nums[index] = 0;
-        index++;
-    }
 };
 
-let nums = [0, 1, 0, 3, 12];
-moveZeroes(nums);
-// console.log(nums);
 
 
 
 
 
 
-// 01. Contains Duplicate
-var containsDuplicate = function(nums) {
-    return new Set(nums).size !== nums.length;
-};
+// //04. Ransom Note
+// var canConstruct = function(ransomNote, magazine) {
+//     let count = {};
+
+//     for (let char of magazine) {
+//         count[char] = (count[char] || 0) + 1;
+//     }
+
+//     for (let char of ransomNote) {
+//         if (!count[char]) {
+//             return false;
+//         }
+
+//         count[char]--;
+//     }
+
+//     return true;
+// };
+
+
+
+
+
+// //03. Valid Anagram
+// var isAnagram = function(s, t) {
+//     return s.split("").sort().join("") === t.split("").sort().join("");
+// };
+
+
+
+
+
+
+
+
+
+
+// //02. Move Zeroes
+// var moveZeroes = function(nums) {
+//     let index = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== 0) {
+//             nums[index] = nums[i];
+//             index++;
+//         }
+//     }
+
+//     while (index < nums.length) {
+//         nums[index] = 0;
+//         index++;
+//     }
+// };
+
+// let nums = [0, 1, 0, 3, 12];
+// moveZeroes(nums);
+// // console.log(nums);
+
+
+
+
+
+
+// // 01. Contains Duplicate
+// var containsDuplicate = function(nums) {
+//     return new Set(nums).size !== nums.length;
+// };
 
