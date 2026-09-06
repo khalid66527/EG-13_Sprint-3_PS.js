17ccb4ecc53db8d4dfc4847788eed55dc6026d7e
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-06 20:15:24 +0600

    04. Ransom Note

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,4 +1,29 @@
 
+
+
+//04. Ransom Note
+var canConstruct = function(ransomNote, magazine) {
+    let count = {};
+
+    for (let char of magazine) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of ransomNote) {
+        if (!count[char]) {
+            return false;
+        }
+
+        count[char]--;
+    }
+
+    return true;
+};
+
+
+
+
+
 //03. Valid Anagram
 var isAnagram = function(s, t) {
     return s.split("").sort().join("") === t.split("").sort().join("");
@@ -13,34 +38,34 @@ var isAnagram = function(s, t) {
 
 
 
-// //02. Move Zeroes
-// var moveZeroes = function(nums) {
-//     let index = 0;
+//02. Move Zeroes
+var moveZeroes = function(nums) {
+    let index = 0;
 
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] !== 0) {
-//             nums[index] = nums[i];
-//             index++;
-//         }
-//     }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
 
-//     while (index < nums.length) {
-//         nums[index] = 0;
-//         index++;
-//     }
-// };
+    while (index < nums.length) {
+        nums[index] = 0;
+        index++;
+    }
+};
 
-// let nums = [0, 1, 0, 3, 12];
-// moveZeroes(nums);
-// // console.log(nums);
-
-
+let nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+// console.log(nums);
 
 
 
 
-// // 01. Contains Duplicate
-// var containsDuplicate = function(nums) {
-//     return new Set(nums).size !== nums.length;
-// };
+
+
+// 01. Contains Duplicate
+var containsDuplicate = function(nums) {
+    return new Set(nums).size !== nums.length;
+};
 
