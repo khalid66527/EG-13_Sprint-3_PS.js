bb4c0a4356b13d07608685bf0522b73135fe188d
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-06 20:21:50 +0600

    09. Longest Consecutive Sequence

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,3 +1,30 @@
+//09. Longest Consecutive Sequence
+var longestConsecutive = function(nums) {
+    let set = new Set(nums);
+    let longest = 0;
+
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            let current = num;
+            let count = 1;
+
+            while (set.has(current + 1)) {
+                current++;
+                count++;
+            }
+
+            longest = Math.max(longest, count);
+        }
+    }
+
+    return longest;
+};
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
+
+
+
+
 
 //08. Top K Frequent Elements
 var topKFrequent = function(nums, k) {
