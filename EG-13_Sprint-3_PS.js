785e143be7fa1301124738884fa1a2d09f9164c0
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-06 20:13:26 +0600

    //03. Valid Anagram

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,32 +1,46 @@
 
-//02. Move Zeroes
-var moveZeroes = function(nums) {
-    let index = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            nums[index] = nums[i];
-            index++;
-        }
-    }
-
-    while (index < nums.length) {
-        nums[index] = 0;
-        index++;
-    }
+//03. Valid Anagram
+var isAnagram = function(s, t) {
+    return s.split("").sort().join("") === t.split("").sort().join("");
 };
 
-let nums = [0, 1, 0, 3, 12];
-moveZeroes(nums);
-// console.log(nums);
 
 
 
 
 
 
-// 01. Contains Duplicate
-var containsDuplicate = function(nums) {
-    return new Set(nums).size !== nums.length;
-};
+
+
+
+// //02. Move Zeroes
+// var moveZeroes = function(nums) {
+//     let index = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== 0) {
+//             nums[index] = nums[i];
+//             index++;
+//         }
+//     }
+
+//     while (index < nums.length) {
+//         nums[index] = 0;
+//         index++;
+//     }
+// };
+
+// let nums = [0, 1, 0, 3, 12];
+// moveZeroes(nums);
+// // console.log(nums);
+
+
+
+
+
+
+// // 01. Contains Duplicate
+// var containsDuplicate = function(nums) {
+//     return new Set(nums).size !== nums.length;
+// };
 
